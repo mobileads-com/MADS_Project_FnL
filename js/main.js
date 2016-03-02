362ef48d43ad45b53ca130e30016155f2f219c0f
reggie <reggie@mobileads.com>
2016-03-02 19:50:50 +0800

    Additional update on animations.

--- js/main.js
+++ js/main.js
@@ -172,9 +172,9 @@ fnl.prototype.firstFrame = function(){
 	_this.parent.innerHTML += '<div class="first-frame">' +
 			'<p class="text-header text-center animated slideInDown">Rebutkan Peluang untuk mendapatkan biasiswa</p>' +
 			'<img src="img/logo.png" alt="FairNLovely" class="logo animated fadeIn slideInDown_halfsec">' +
-			'<p class="text-currency text-center animated fadeIn_1sec">RM200,000*</p>' +
-			'<p class="text-currency-label text-center animated fadeIn_1sec">JADIKAN IMPIANMU KENYATAAN</p>' +
-			'<p class="text-content text-center animated fadeIn_1sec">Beritahu kami berapa banyak<br>yang anda mahukan biasiswa ini</p>' +
+			'<p class="text-currency text-center animated fadeIn_1halfsec">RM200,000*</p>' +
+			'<p class="text-currency-label text-center animated fadeIn_1halfsec">JADIKAN IMPIANMU KENYATAAN</p>' +
+			'<p class="text-content text-center animated fadeIn_1halfsec">Beritahu kami berapa banyak<br>yang anda mahukan biasiswa ini</p>' +
 			'<p class="text-click text-center animated fadeIn_2sec">Klik di sini untuk mula</p>' +
 			'<div class="arrow-shadow"></div>' +
 			'<img src="img/arrow_down.png" alt="arrow" class="arrow-down animated fadeIn_2halfsec">' +
@@ -184,7 +184,7 @@ fnl.prototype.firstFrame = function(){
 
 	setTimeout(function(){
 		document.querySelector('.arrow-down').setAttribute('class', 'arrow-down slideInDown_inf');
-	}, 4000);
+	}, 3500);
 
 	var nextHandler = function(){
 		_this.app.tracker('E', 'klik_disini');
